fd35458b23d4cd1feef1e23e27e3d74df22bdcf2
fuzhongyi <fuzy450@foxmail.com>
2017-11-09 17:41:22 +0800

    :rocket: project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import {MyApp} from './app.component';
 
-import {AboutPage} from '../pages/about/about';
-import {ContactPage} from '../pages/contact/contact';
 import {HomePage} from '../pages/home/home';
-import {TabsPage} from '../pages/tabs/tabs';
-import {LoginPage} from '../pages/login/login';
 
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
@@ -16,11 +12,7 @@ import {Camera} from '@ionic-native/camera';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    LoginPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -29,11 +21,7 @@ import {Camera} from '@ionic-native/camera';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    LoginPage
+    HomePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +9,17 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  getPos() {
+    alert(JSON.stringify(navigator.geolocation));
+    navigator.geolocation.getCurrentPosition(
+      (resp) => {
+        console.log(JSON.stringify(resp));
+      },
+      (error) => {
+        console.log(JSON.stringify(error));
+      })
   }
 
 }
